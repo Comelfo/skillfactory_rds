39b4a982f831b5b557b7472f135b2eaa780d115b
Comelfo <oriolecorp@yandex.ru>
2020-04-29 00:14:49 +0300

    Guess the Number

--- module_0/main.py
+++ module_0/main.py
@@ -1,1 +1,13 @@
-print('Hello world')
+import numpy as np
+count = 0                            # счетчик попыток
+number = np.random.randint(1,101)    # загадали число
+print (Загадано число от 1 до 100)
+
+while True:                        # бесконечный цикл
+    predict = int(input())         # предполагаемое число
+    count += 1                     # плюсуем попытку
+    if number == predict: break    # выход из цикла, если угадали
+    elif number > predict: print (fУгадываемое число больше {predict} )
+    elif number < predict: print (fУгадываемое число меньше {predict} )
+        
+print (fВы угадали число {number} за {count} попыток.)
